cff0b75143f29d707e96ee04007f41e727551861
Эльмир <uuum1984@mail.ru>
2025-03-20 13:03:32 +0300

    123

--- app/Http/Controllers/SotrController.php
+++ app/Http/Controllers/SotrController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use \Maatwebsite\Excel\Facades\Excel;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Table;
+use PhpOffice\PhpSpreadsheet\Worksheet\Table\TableStyle;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class SotrController extends Controller
 {
@@ -16,9 +19,40 @@ class SotrController extends Controller
             ->get();
         return view('office_program.lab3.index', compact('sotrs'));
     }
-
     public function createExcel()
     {
-        Excel::
+        require_once('../../vendor/autoload.php');
+        $sotr = DB::table('sotr')
+            ->join('otdels', 'sotr.Otdel', '=', 'otdels.idOtdel')
+            ->join('zarpl', 'sotr.id', '=', 'zarpl.idSotr')
+            ->get();
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('C1', 'id');
+        $sheet->setCellValue('D1', 'Отдел');
+        $sheet->setCellValue('E1', 'Фамилия');
+        $sheet->setCellValue('F1', 'Имя');
+        $sheet->setCellValue('G1', 'Зарплата');
+        print_r($sotr);
+        for ($i=0; $i<count($sotr); $i++) {
+            $sheet->setCellValue('C'.$i+2, $sotr[$i]["id"]);
+            $sheet->setCellValue('D'. $i+2, $sotr[$i]["NameOtdel"]);
+            $sheet->setCellValue('E'. $i+2, $sotr[$i]["LastName"]);
+            $sheet->setCellValue('F'. $i+2, $sotr[$i]["FirstName"]);
+            $sheet->setCellValue('G'.$i+2, $sotr[$i]["TotalSalary"]);
+        };
+
+        $writer = new Xlsx($spreadsheet);
+
+        $table = new Table('A1:L50', 'Table1');
+
+        $tableStyle = new TableStyle();
+        $tableStyle->setTheme(TableStyle::TABLE_STYLE_MEDIUM9);
+        $tableStyle->setShowRowStripes(true);
+        $table->setStyle($tableStyle);
+
+        $sheet->addTable($table);
+
+        $writer->save('CreateExcelTable.xlsx');
     }
 }
